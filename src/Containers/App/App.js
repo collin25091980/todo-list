31d8d692b7a746030f539474e5c2702b715c821f
Sebastien Collin <sebastien.collin91100@gmail.com>
2023-04-11 19:32:45 +0200

    Projet fini

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import classes from './App.module.css';
 import Task from '../../Components/Task/Task';
+import axios from '../../axios-firebase';
 
 function App() {
 
@@ -8,17 +9,49 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [input, setInput] = useState('');
 
+  // Ref
+  const inputRef = useRef('');
+  
+  // useEffect
+  useEffect(() => {
+    inputRef.current.focus();
+
+    // Récupérer les taches
+    fetchTasks();
+
+
+  },[]);
+
+
   // Fonctions
   const removeClickedHandler = index => {
     const newTasks = [...tasks];
     newTasks.splice(index, 1);
     setTasks(newTasks);
+
+    axios.delete('/taches/' + tasks[index].id + '.json')
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+
+
   }
 
   const doneClickedHandler = index => {
     const newTasks = [...tasks];
     newTasks[index].done = !tasks[index].done;
     setTasks(newTasks);
+
+    axios.put('/taches/' + tasks[index].id + '.json', tasks[index])
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      })
   }
 
   const submittedTaskHandler = event => {
@@ -28,13 +61,42 @@ function App() {
       content: input,
       done: false
     }
+    
     setTasks([...tasks, newTask]);
     setInput('');
+
+    axios.post('/taches.json', newTask)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+
   }
 
   const changedFormHandler = event => {
     setInput(event.target.value);
   }
+
+  const fetchTasks = () => {
+    axios.get('/taches.json')
+      .then(response => {
+        const newTasks = [];
+        for(let key in response.data) {
+          newTasks.push({
+            ...response.data[key],
+            id: key
+          });
+        }
+        setTasks(newTasks);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+
 
   // Variables
   let tasksDisplayed = tasks.map((task, index) => (
@@ -69,7 +131,9 @@ function App() {
             type="text"
             value={input}
             onChange={(e) => changedFormHandler(e)}
-            placeholder="Que souhaitez-vous ajouter ?" />
+            placeholder="Que souhaitez-vous ajouter ?"
+            ref={inputRef}
+          />
           <button type="submit">
             Ajouter
           </button>
